5d3573b4685764068f870cd9bef864fdf78683af
rohithvaidya <rohithvaidya@gmail.com>
2025-09-24 10:35:20 +0530

    adding use case files

---
+++ use_cases/weighted_histogram/aks_client_3.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+python3 client3.py
+
+# Input file with data (one number per line)
+input_file="processed_output_3.txt"
+
+# Read all values into an array
+values=($(cat "$input_file"))
+
+# Counter for naming export variables
+counter=1
+
+# Total number of values
+total=${#values[@]}
+
+# Loop over all values, 4 at a time
+for ((i=0; i<total; i+=4)); do
+    val1=${values[i]:-}
+    val2=${values[i+1]:-}
+    val3=${values[i+2]:-}
+    val4=${values[i+3]:-}
+
+    cmd="java -jar cs.jar amphora create-secret"
+    [[ -n $val1 ]] && cmd+=" $val1"
+    [[ -n $val2 ]] && cmd+=" $val2"
+    [[ -n $val3 ]] && cmd+=" $val3"
+    [[ -n $val4 ]] && cmd+=" $val4"
+    cmd+=" -t hospital=Artemis"
+
+    export_var="ARTEMIS_DATA_ID_$counter"
+
+    # Echo the command before running it
+    echo "Running: export $export_var=\$($cmd)"
+
+    # Actually run and export
+    export "$export_var=$($cmd)"
+    echo "$export_var: ${!export_var}"
+
+    ((counter++))
+done
+
---
+++ use_cases/weighted_histogram/execute2.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Step 1: Get all secret UUIDs
+java -jar cs.jar amphora get-secrets > input.txt
+
+# Step 2: Read all UUIDs into an array
+mapfile -t uuids < <(grep -Eo '([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})' input.txt)
+
+# Step 3: Build the -i arguments for each UUID
+input_args=()
+for uuid in "${uuids[@]}"; do
+  input_args+=("-i" "$uuid")
+done
+
+# Step 4: Calculate the total input length for the MPC program
+count=${#uuids[@]}
+input_length=$((count * 100))
+
+echo "UUID count: $count"
+echo "Total input length (500 * count): $input_length"
+
+# Step 5: Create the binning.mpc file with dynamic input length
+cat << EOF > binning.mpc
+# Prologue to read in the inputs
+port=regint(10000)
+listen(port)
+socket_id = regint()
+acceptclientconnection(socket_id, port)
+v = sint.read_from_socket(socket_id, $input_length)
+
+# The logic
+y = Array(5, sint)
+z = Array(5, sint)
+for i in range(5):
+    for j in range(0, $input_length, 2):
+        check = (0 + i*20 <= v[j]) 
+        check2 = (v[j] < 20 + i*20)
+        y[i] = y[i] + check*check2*v[j+1]
+
+# Epilogue to return the outputs 
+resp = Array(5, sint)
+for i in range(5):
+    resp[i] = y[i]
+
+sint.write_to_socket(socket_id, resp)
+EOF
+
+# Step 6: Execute the ephemeral task using all -i arguments
+cat binning.mpc | java -jar cs.jar ephemeral execute \
+  "${input_args[@]}" \
+  ephemeral-generic.default \
+  | tail -n +2 \
+  | sed 's/[][]//g'
+
+# Step 7: Print the result ID
+#echo "Result ID: $RESULT_ID"
+#echo "$RESULT_ID" > result_id.txt
+# java -jar cs.jar amphora get-secret $RESULT_ID
