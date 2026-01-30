59c7a72ec69491830cd8cac18e9b44ad5a322b3e
samratacharya-iudx <samrat.acharya@datakaveri.org>
2026-01-30 16:23:56 +0530

    Delete use_cases/Fingercode directory
    reordering files
    
    
    Signed-off-by: samratacharya-iudx <samrat.acharya@datakaveri.org>

--- use_cases/Fingercode/preprocess.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for file in *features.txt; do
-	value=($(cat "$file"))
-	values="${value[*]}"
-	echo "$values" > "preprocessed_$file"
-done
--- use_cases/Fingercode/recrop.m
+++
@@ -1,7 +0,0 @@
-function [out]=recrop(in,dimxt,dimyt)
-% recroppping
-
-extx=20;
-exty=20;
-
-out=in(extx+1:extx+dimxt,exty+1:exty+dimyt);
